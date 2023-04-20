6a225b1c2e9e18bff01182b787ef3aa4e329cfad
jburke01 <jburke26@amherst.edu>
2023-04-20 14:05:12 -0400

    join the map to graph nhl

--- NHLwrangled.R
+++ NHLwrangled.R
@@ -2,23 +2,43 @@
 #import packages
 library(maps)
 library(tidyverse)
+library(sf)
 
 #import the nhl draft dataset
 nhl <- read_csv("dataforNHL/nhldraft.csv")
+
+#import the map
+world_map <- maps::map("world", plot = FALSE, fill = TRUE) %>%
+  st_as_sf()
 
 #mutate to change nationalities to coutries on maps
 nhlcountrynames <- nhl %>%
   mutate_if(is.numeric, ~replace_na(.,0))%>%
   mutate(nationality = ifelse(nationality == "CA", paste("Canada"), nationality ))%>%
-  mutate(nationality = ifelse(nationality == "US", paste("United States"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "US", paste("USA"), nationality ))%>%
   mutate(nationality = ifelse(nationality == "FI", paste("Finland"), nationality ))%>%
   mutate(nationality = ifelse(nationality == "SE", paste("Sweden"), nationality ))%>%
   mutate(nationality = ifelse(nationality == "RU", paste("Russia"), nationality ))%>%
-  mutate(nationality = ifelse(nationality == "CZ", paste("Czech"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "CZ", paste("Czech Republic"), nationality ))%>%
   mutate(nationality = ifelse(nationality == "FR", paste("France"), nationality ))%>%
   mutate(nationality = ifelse(nationality == "CH", paste("Switzerland"), nationality ))%>%
   mutate(nationality = ifelse(nationality == "DE", paste("Germany"), nationality ))%>%
-  mutate(nationality = ifelse(nationality == "SK", paste("Slovakia"), nationality ))
+  mutate(nationality = ifelse(nationality == "SK", paste("Slovakia"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "AT", paste("Austrian"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "AU", paste("Australia"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "DK", paste("Denmark"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "LV", paste("Latvia"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "UA", paste("Ukraine"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "ZA", paste("South Africa"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "EE", paste("Estonia"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "SI", paste("Slovenia"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "NL", paste("Netherlands"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "BY", paste("Belarus"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "GB", paste("UK"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "IT", paste("Italy"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "KZ", paste("Kazakhstan"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "NO", paste("Norway"), nationality ))%>%
+  mutate(nationality = ifelse(nationality == "PL", paste("Poland"), nationality ))
 
 
 
@@ -51,3 +71,8 @@ nhlmosteffectivetotal1 <- nhlmosteffective %>%
 nhlmosteffectiveskater1 <- nhlmosteffectivetotal1 %>%
   mutate(valueofskater = winshares * overall_pick * ppg)%>%
   arrange(desc(valueofskater))
+
+# combine the map to the countries 
+nationalitymap <- world_map %>%
+  left_join(nhlmosteffectiveskater1, by = c("ID" = "nationality"), multiple = "all")%>%
+  mutate_if(is.numeric, ~replace_na(.,0))
